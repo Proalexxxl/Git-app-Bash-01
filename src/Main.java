91eba440f96a3f0c9533b38d86bad9d8c878ee10
Alex <proalexxxl@gmail.com>
2022-02-20 11:49:42 +0200

    Change greeting in master class Main

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main (String[] args) {
-        System.out.println("My git app. Super!");
+        System.out.println("My Super App!");
 
         int a = 5;
         int b = 7;
